e002e9af104527d46abd6c3820e602302e2b3902
messi <messi@qq.com>
2022-04-23 08:57:32 +0800

    order.html works

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from base.views import *
+from . import views
+
 
 urlpatterns = [
     path('', home),
+    path('orders/', OrderView.as_view()),
+    path('order', order_detail)
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Customer, Order
@@ -5,3 +6,23 @@ from .models import Customer, Order
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+class OrderView(TemplateView):
+    template_name = 'order_list.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        data = Order.objects.all()
+        context['order_list'] = data
+        return context
+
+
+def order_detail(request):
+    num = request.GET.get('id', 1)
+    print(num)
+    order = Order.objects.get(id=num)
+    context = {
+        'order': order
+    }
+    return render(request, 'order.html', context)
